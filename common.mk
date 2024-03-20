c5ccad2c53d635673b7437ec348e8e4fd11bd8d9
kijodoari <kijodoari@gmail.com>
2024-03-20 14:54:18 +0100

    sm8350-common: Enable UDFPS icons and animations
    Signed-off-by: kijodoari <kijodoari@gmail.com>

--- common.mk
+++ common.mk
@@ -179,6 +179,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+TARGET_HAS_UDFPS := true
+
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
